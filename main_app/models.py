cbc82bfdb4909d38a6d3339a8b953b3d76e2cab7
Alexandrea <lexhom24@gmail.com>
2022-08-09 11:48:39 -0400

    Add wine list

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Wine(models.Model):
+  name = models.CharField(max_length=100)
+  brand = models.CharField(max_length=100)
+  taste = models.TextField(max_length=250)
+  year = models.IntegerField()
+
+def __str__(self):
+    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Wine
 
-# Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def wines_index(request):
+  wines = Wine.objects.all()
+  return render(request, 'wines/index.html', { 'wines': wines })
+
+def wines_detail(request, wine_id):
+  wine = Wine.objects.get(id=wine_id)
+  return render(request, 'wines/detail.html', { 'wine': wine })
